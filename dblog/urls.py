2a7c72a4d06857ae3572878aa1586ce2d21a1ae4
davila7 <dan.avila7@gmail.com>
2015-03-24 13:17:09 -0300

    static url 2

--- dblog/urls.py
+++ dblog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from blog.views import IndexView, EntradaDetailView
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
