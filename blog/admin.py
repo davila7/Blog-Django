bf8eb2b60fe5d44fd011e3f05e429767a83293bc
davila7 <dan.avila7@gmail.com>
2015-03-24 18:47:53 -0300

    editor de texto

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from .models import Entrada
 from django_markdown.admin import MarkdownModelAdmin
+from django_markdown.widgets import AdminMarkdownWidget
+from django.db.models import TextField
 
 
 @admin.register(Entrada)
 class EntradaAdmin(MarkdownModelAdmin):
 	list_display = ("titulo", "created")
 	propopulated_fields = {"slug":("titulo",)}
+	# Next line is a workaround for Python 2.x
+	formfield_overrides = {TextField: {'widget': AdminMarkdownWidget}}
 
 
